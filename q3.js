5d7965c09bef338f61c5fc933479241f3f4c478f
Aganya Bajaj <aganyabajaj7@gmail.com>
2023-01-15 23:37:10 +0530

    chore: change character case in string

---
+++ q3.js
@@ -0,0 +1,9 @@
+function changeCase(str) {
+    ans = [...str].map(x => (x == x.toLowerCase()) ? x = x.toUpperCase() : x = x.toLowerCase())
+    return ans.toString()
+}
+
+const changeCaseA = (str) => {
+    ans = [...str].map(x => (x == x.toLowerCase()) ? x = x.toUpperCase() : x = x.toLowerCase())
+    return ans.toString()
+}
