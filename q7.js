6ef3873e2aa717973f07b0c9df246372c298c53f
Aganya Bajaj <aganyabajaj7@gmail.com>
2023-01-16 00:13:34 +0530

    chore: concat two strings without their first letters

---
+++ q7.js
@@ -0,0 +1,11 @@
+function concat (str1, str2) {
+    str1 = str1.slice(1)
+    str2 = str2.slice(1)
+    return str1.concat(str2)
+}
+
+const concatA = (str1, str2) => {
+    str1 = str1.slice(1)
+    str2 = str2.slice(1)
+    return str1.concat(str2)
+}
